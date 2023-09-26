1b425f538536b35de6036a8a50449aeb271456b3
prasad <pansareprasad3798@gmail.com>
2023-09-26 19:47:08 +0530

    configuring service registry(Eureka server)

--- user-services/src/main/java/com/micoservices/user/UserServicesApplication.java
+++ user-services/src/main/java/com/micoservices/user/UserServicesApplication.java
@@ -2,9 +2,10 @@ package com.micoservices.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class UserServicesApplication {
 
 	public static void main(String[] args) {
