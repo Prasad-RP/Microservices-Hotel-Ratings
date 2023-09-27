d7d26e73fc97a1c8f2393e15d42ec3699e4c39c5
prasad <pansareprasad3798@gmail.com>
2023-09-27 19:43:57 +0530

    configuring service registry(Eureka server)

--- rating-services/src/main/java/com/micoservices/rating/RatingServicesApplication.java
+++ rating-services/src/main/java/com/micoservices/rating/RatingServicesApplication.java
@@ -2,8 +2,10 @@ package com.micoservices.rating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingServicesApplication {
 
 	public static void main(String[] args) {
